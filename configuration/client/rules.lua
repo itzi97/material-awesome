034eea45822b59868c1a247886be8ce7faaa7d40
Itziar Morales Rodríguez <itzimr97@outlook.com>
2020-11-06 18:19:12 +0100

    Fixed some floating window rules + added default program rules

--- configuration/client/rules.lua
+++ configuration/client/rules.lua
@@ -1,7 +1,7 @@
-local awful = require('awful')
-local gears = require('gears')
-local client_keys = require('configuration.client.keys')
-local client_buttons = require('configuration.client.buttons')
+local awful = require("awful")
+local gears = require("gears")
+local client_keys = require("configuration.client.keys")
+local client_buttons = require("configuration.client.buttons")
 
 -- Rules
 awful.rules.rules = {
@@ -22,27 +22,28 @@ awful.rules.rules = {
       ontop = false,
       sticky = false,
       maximized_horizontal = false,
-      maximized_vertical = false
-    }
-  },
+      maximized_vertical = false,
+    },
+  }, {rule_any = {name = {"QuakeTerminal"}}, properties = {skip_decoration = true}}, -- Titlebars
   {
-    rule_any = {name = {'QuakeTerminal'}},
-    properties = {skip_decoration = true}
-  },
-  -- Titlebars
-  {
-    rule_any = {type = {'dialog'}, class = {'Wicd-client.py', 'calendar.google.com'}},
+    rule_any = {
+      type = {"dialog"},
+      class = {"Wicd-client.py", "calendar.google.com", "Toggl Desktop", "Clockify"},
+    },
     properties = {
       placement = awful.placement.centered,
       ontop = true,
       floating = true,
       drawBackdrop = true,
-      shape = function()
-        return function(cr, w, h)
-          gears.shape.rounded_rect(cr, w, h, 8)
-        end
-      end,
-      skip_decoration = true
-    }
-  }
+      shape = function() return function(cr, w, h) gears.shape.rounded_rect(cr, w, h, 8) end end,
+      skip_decoration = true,
+    },
+  }, {
+    rule_any = {class = {"MEGAsync"}},
+    properties = {ontop = true, floating = true, drawBackdrop = false, skip_decoration = true},
+  }, -- Move default programs to their tag
+  {rule_any = {class = {"firefox", "Brave-browser"}}, properties = {tag = "1"}},
+  {rule_any = {class = {"discord", "Thunderbird"}}, properties = {tag = "3"}},
+  {rule_any = {class = {"Spotify"}}, properties = {tag = "5"}},
+  {rule_any = {class = {"Steam"}}, properties = {tag = "6"}},
 }
