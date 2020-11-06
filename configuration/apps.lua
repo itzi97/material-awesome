e69cd32dc596e77961da2fd3f517d955bbd37963
Itziar Morales Rodríguez <itzimr97@outlook.com>
2020-11-06 11:25:04 +0100

    Added blur with compton tryone fork

--- configuration/apps.lua
+++ configuration/apps.lua
@@ -15,15 +15,15 @@ return {
     rofi = "rofi -dpi " .. get_dpi() .. " -width " .. with_dpi(400) .. " -show drun -theme " ..
       filesystem.get_configuration_dir() .. "/configuration/rofi.rasi",
     lock = "i3lock-fancy-rapid 5-3 -k --timecolor=ffffffff --datecolor=ffffffff",
-    quake = "alacritty --title QuakeTerminal"
+    quake = "alacritty --title QuakeTerminal",
   },
   -- List of apps to start once on start-up
   run_on_start_up = {
-    "picom --config " .. filesystem.get_configuration_dir() .. "/configuration/compton.conf",
+    "compton --config " .. filesystem.get_configuration_dir() .. "/configuration/compton.conf",
     "blueberry-tray", -- Bluetooth tray icon
     "xfce4-power-manager", -- Power manager
     "/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 & eval $(gnome-keyring-daemon -s --components=pkcs11,secrets,ssh,gpg)", -- credential manager
     "nitrogen --restore", -- Reload background
-    "piactl connect" -- Load VPN
-  }
+    "piactl connect", -- Load VPN
+  },
 }
