887d21491fefd292d81e06aa8f45d97c3c5e2571
Itziar Morales Rodríguez <itzimr97@outlook.com>
2020-11-06 11:37:18 +0100

    Modified some default programs

--- configuration/tags/init.lua
+++ configuration/tags/init.lua
@@ -1,71 +1,32 @@
-local awful = require('awful')
-local gears = require('gears')
-local icons = require('theme.icons')
+local awful = require("awful")
+local gears = require("gears")
+local icons = require("theme.icons")
 
 local tags = {
-  {
-    icon = icons.chrome,
-    type = 'chrome',
-    defaultApp = 'google-chrome-beta',
-    screen = 1
-  },
-  {
-    icon = icons.code,
-    type = 'code',
-    defaultApp = 'code',
-    screen = 1
-  },
-  {
-    icon = icons.social,
-    type = 'social',
-    defaultApp = 'station',
-    screen = 1
-  },
-  {
-    icon = icons.folder,
-    type = 'files',
-    defaultApp = 'nautilus',
-    screen = 1
-  },
-  {
-    icon = icons.music,
-    type = 'music',
-    defaultApp = 'youtube-music',
-    screen = 1
-  },
-  {
-    icon = icons.game,
-    type = 'game',
-    defaultApp = '',
-    screen = 1
-  },
-  {
-    icon = icons.lab,
-    type = 'any',
-    defaultApp = '',
-    screen = 1
-  }
+  {icon = icons.chrome, type = "chrome", defaultApp = "brave", screen = 1},
+  {icon = icons.code, type = "code", defaultApp = "code", screen = 1},
+  {icon = icons.social, type = "social", defaultApp = "discord", screen = 1},
+  {icon = icons.folder, type = "files", defaultApp = "nautilus", screen = 1},
+  {icon = icons.music, type = "music", defaultApp = "spotify", screen = 1},
+  {icon = icons.game, type = "game", defaultApp = "steam", screen = 1},
+  {icon = icons.lab, type = "any", defaultApp = "", screen = 1},
 }
 
-awful.layout.layouts = {
-  awful.layout.suit.tile,
-  awful.layout.suit.max
-}
+awful.layout.layouts = {awful.layout.suit.tile, awful.layout.suit.max}
 
 awful.screen.connect_for_each_screen(
   function(s)
     for i, tag in pairs(tags) do
       awful.tag.add(
-        i,
-        {
+        i, {
           icon = tag.icon,
           icon_only = true,
           layout = awful.layout.suit.tile,
           gap_single_client = false,
-          gap = 4,
+          gap = 8,
           screen = s,
           defaultApp = tag.defaultApp,
-          selected = i == 1
+          selected = i == 1,
         }
       )
     end
@@ -73,9 +34,8 @@ awful.screen.connect_for_each_screen(
 )
 
 _G.tag.connect_signal(
-  'property::layout',
-  function(t)
-    local currentLayout = awful.tag.getproperty(t, 'layout')
+  "property::layout", function(t)
+    local currentLayout = awful.tag.getproperty(t, "layout")
     if (currentLayout == awful.layout.suit.max) then
       t.gap = 0
     else
--- configuration/tags/layouts/single-maximized.lua
+++ configuration/tags/layouts/single-maximized.lua
@@ -5,7 +5,6 @@
 -- @copyright 2008 Julien Danjou
 -- @module awful.layout
 ---------------------------------------------------------------------------
-
 -- Grab environment we need
 local pairs = pairs
 
@@ -22,35 +21,27 @@ local max = {}
 -- @see gears.surface
 
 local function fmax(p, fs)
-    -- Fullscreen?
-    local area
-    if fs then
-        area = p.geometry
-    else
-        area = p.workarea
-    end
-    local focused_client = client.focus
-    for _, c in pairs(p.clients) do
-        local g = {
-            x = area.x,
-            y = area.y,
-            width = area.width,
-            height = area.height
-        }
-        p.geometries[c] = g
-    end
+  -- Fullscreen?
+  local area
+  if fs then
+    area = p.geometry
+  else
+    area = p.workarea
+  end
+  local focused_client = client.focus
+
+  for _, c in pairs(p.clients) do
+    local g = {x = area.x, y = area.y, width = area.width, height = area.height}
+    p.geometries[c] = g
+  end
 end
 
 --- Maximized layout.
 -- @clientlayout awful.layout.suit.max.name
-max.name = 'max'
-function max.arrange(p)
-    return fmax(p, false)
-end
+max.name = "max"
+function max.arrange(p) return fmax(p, false) end
 function max.skip_gap(nclients, t) -- luacheck: no unused args
-    return true
+  return true
 end
 
 return max
-
--- vim: filetype=lua:expandtab:shiftwidth=4:tabstop=8:softtabstop=4:textwidth=80
